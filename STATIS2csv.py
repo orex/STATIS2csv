e0ca3d6516f406cd77788c36c48e8d627edc9e0c
Kirill Okhotnikov <kirill@headnode.hpc.local>
2014-10-17 10:14:30 +0200

    Unix format output support added.

--- STATIS2csv.py
+++ STATIS2csv.py
@@ -4,7 +4,7 @@ import csv
 import argparse
 
 # columns > 27 are unknown when only considering the STATIS file
-csv_header = ('md step','simulation time [ps]','total energy','system temperature','configurational energy','VdW energy','coulombic energy','bond energy','angle energy','dihedral energy','tethering energy','enthalpy','rotational temperature','total virial','VdW virial','coulombic virial','bond virial','angle virial','constraint virial','tethering virial','volume','core-shell temperature','core-shell potential energy','core-shell virial','cell angle alpha','cell angle beta','cell angle gamma','PMF constraint virial','pressure')
+csv_header = ('#md step','simulation time [ps]','total energy','system temperature','configurational energy','VdW energy','coulombic energy','bond energy','angle energy','dihedral energy','tethering energy','enthalpy','rotational temperature','total virial','VdW virial','coulombic virial','bond virial','angle virial','constraint virial','tethering virial','volume','core-shell temperature','core-shell potential energy','core-shell virial','cell angle alpha','cell angle beta','cell angle gamma','PMF constraint virial','pressure')
 
 def read_STATIS(STATIS):
     # first two lines with configuration name
@@ -44,11 +44,21 @@ def read_STATIS(STATIS):
 
     # return values
     return records
+  
+
+class unix_tab(csv.excel_tab):
+    """Describe the usual properties of Excel-generated TAB-delimited files."""
+    delimiter = '\t'
+    lineterminator = '\n'
+csv.register_dialect("unix-tab", unix_tab)  
+
+dialects_list = ', '.join(map(str, csv.list_dialects()))
 
 # initialize parser
-parser = argparse.ArgumentParser(description='Converts a DL_POLY 4 STATIS file to CSV format.')
-parser.add_argument('STATIS', nargs='?', type=str, help='filename of STATIS file, default: STATIS', default='STATIS')
-parser.add_argument('-o', '--out', metavar='CSV', dest='CSV', type=str, help='filename for CSV output, default: STATIS.csv', default='STATIS.csv')
+parser = argparse.ArgumentParser(description='Converts a DL_POLY 4 STATIS file to table format.')
+parser.add_argument('STATIS', nargs='?', type=str, help='filename of STATIS file. Default: %(default)s', default='STATIS')
+parser.add_argument('-o', '--out', metavar='CSV', dest='CSV', type=str, help='filename for CSV output. Default: %(default)s', default='STATIS.csv')
+parser.add_argument('-d', '--dialect', dest='dialect_name', type=str, help='File output format: ' + dialects_list + '. Default: %(default)s', default='unix-tab')
 
 # parse arguments
 args = parser.parse_args()
@@ -59,6 +69,7 @@ with open(args.STATIS, 'r') as STATIS:
 
 # write records to CSV file
 with open(args.CSV, 'w') as CSV:
-    writer = csv.writer(CSV)
+    writer = csv.writer(CSV, dialect=args.dialect_name)
     writer.writerow(csv_header)
     writer.writerows(records)
+
